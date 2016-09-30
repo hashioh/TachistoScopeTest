1d2ed015641a6763e6481e97cf395d06309975e4
kazuki ohashi <kensapo.zbn@gmail.com>
2016-09-30 14:47:18 +0900

    Initial Commit

---
+++ TachistoscoppeTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TachistoscoppeTest
+//
+//  Created by Kazuki Ohashi on 2016/09/30.
+//  Copyright © 2016年 Kazuki Ohashi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
